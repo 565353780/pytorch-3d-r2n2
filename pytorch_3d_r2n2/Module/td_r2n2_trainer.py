d298efa7bdefda8ca31ebbff831d57446a706971
chLi <565353780@qq.com>
2022-09-21 16:53:07 +0800

    fix training bugs

--- pytorch_3d_r2n2/Module/td_r2n2_trainer.py
+++ pytorch_3d_r2n2/Module/td_r2n2_trainer.py
@@ -6,62 +6,90 @@ from datetime import datetime
 
 import torch
 from torch.optim import SGD, Adam, lr_scheduler
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from pytorch_3d_r2n2.Config.config import cfg
-
+from pytorch_3d_r2n2.Dataset.dataset import ShapeNetCollateFn, ShapeNetDataset
 from pytorch_3d_r2n2.Method.time import getCurrentTimeStr
 from pytorch_3d_r2n2.Method.utils import has_nan, max_or_nan
-
 from pytorch_3d_r2n2.Module.td_r2n2_detector import TDR2N2Detector
+
+#  from pytorch_3d_r2n2.Method.process import kill_processes
 
 
 class TDR2N2Trainer(TDR2N2Detector):
 
     def __init__(self, model_file_path=None):
-        super().__init__(model_file_path)
-        self.start_train_step = 0
         self.optimizer = None
-        self.lr_scheduler = None
+        self.scheduler = None
+        self.start_train_step = 0
+
+        super().__init__(model_file_path)
 
         log_folder_path = cfg.LOG.log_folder_path + getCurrentTimeStr() + "/"
         os.makedirs(log_folder_path, exist_ok=True)
         self.writer = SummaryWriter(log_folder_path)
+
+        self.initOptimizer(cfg.TRAIN.POLICY)
+        self.initScheduler(cfg.TRAIN.LEARNING_RATES)
         return
-
-    def loadOptimizer(self, policy):
-        assert policy in ['adam', 'sgd']
-
-        if policy == 'adam':
-            self.optimizer = Adam(self.model.parameters(),
-                                  lr=self.lr,
-                                  weight_decay=cfg.TRAIN.WEIGHT_DECAY)
-        else
-            momentum = cfg.TRAIN.MOMENTUM
-            self.optimizer = SGD(self.model.parameters(),
-                                 lr=self.lr,
-                                 weight_decay=cfg.TRAIN.WEIGHT_DECAY,
-                                 momentum=momentum)
-
-        if self.checkpoint is not None:
-            optim_state = self.checkpoint['optimizer_state']
-            self.optimizer.load_state_dict(optim_state)
-        return True
 
     def loadModel(self, model_file_path):
         if not super().loadModel(model_file_path):
             print("[ERROR][TDR2N2Trainer::loadModel]")
             print("\t loadModel in TDR2N2Detector failed!")
             return False
-
         self.model.train()
-        if not self.loadOptimizer(cfg.TRAIN.POLICY):
+
+        self.initOptimizer(cfg.TRAIN.POLICY)
+        self.initScheduler(cfg.TRAIN.LEARNING_RATES)
+
+        if not self.loadOptimizer():
             print("[ERROR][TDR2N2Trainer::loadModel]")
             print("\t loadOptimizer failed!")
             return False
 
-        self.start_train_step = self.checkpoint['train_step']
+        if 'train_step' in self.checkpoint.keys():
+            self.start_train_step = self.checkpoint['train_step']
+            print("[INFO][TDR2N2Trainer::loadModel]")
+            print("\t start training from train_step = " +
+                  str(self.start_train_step) + "...")
+        return True
+
+    def initOptimizer(self, policy):
+        if self.optimizer is not None:
+            return True
+
+        assert policy in ['adam', 'sgd']
+
+        if policy == 'adam':
+            self.optimizer = Adam(self.model.parameters(),
+                                  lr=self.lr,
+                                  weight_decay=cfg.TRAIN.WEIGHT_DECAY)
+        else:
+            momentum = cfg.TRAIN.MOMENTUM
+            self.optimizer = SGD(self.model.parameters(),
+                                 lr=self.lr,
+                                 weight_decay=cfg.TRAIN.WEIGHT_DECAY,
+                                 momentum=momentum)
+        return True
+
+    def initScheduler(self, lr_dict):
+        if self.scheduler is not None:
+            return True
+
+        lr_steps = [int(k) for k in lr_dict.keys()]
+        self.scheduler = lr_scheduler.MultiStepLR(self.optimizer,
+                                                  lr_steps,
+                                                  gamma=0.1)
+        return True
+
+    def loadOptimizer(self):
+        if self.checkpoint is not None:
+            optim_state = self.checkpoint['optimizer_state']
+            self.optimizer.load_state_dict(optim_state)
         return True
 
     def saveModel(self, step):
@@ -86,7 +114,6 @@ class TDR2N2Trainer(TDR2N2Detector):
 
     def trainStep(self, images, voxels):
         data = {'inputs': {}, 'predictions': {}, 'losses': {}, 'logs': {}}
-
         data['inputs']['images'] = images.cuda()
         data['inputs']['voxels'] = voxels.cuda()
 
@@ -99,30 +126,27 @@ class TDR2N2Trainer(TDR2N2Detector):
 
     def valStep(self, val_loader):
         val_losses = 0
-        val_num_iter = min(cfg.TRAIN.NUM_VALIDATION_ITERATIONS, len(val_loader))
+        val_num_iter = min(cfg.TRAIN.NUM_VALIDATION_ITERATIONS,
+                           len(val_loader))
         val_loader_iter = iter(val_loader)
         for _ in range(val_num_iter):
             batch_img, batch_voxel = val_loader_iter.next()
-            data = self.trainStep(batch_img, batch_voxel)
+
+            data = {'inputs': {}, 'predictions': {}, 'losses': {}, 'logs': {}}
+            data['inputs']['images'] = batch_img.cuda()
+            data['inputs']['voxels'] = batch_voxel.cuda()
+
+            data = self.model(data)
+
             val_loss = data['losses']['loss']
             val_losses += val_loss
         val_losses_mean = val_losses / val_num_iter
         return val_losses_mean
 
     def train(self, train_loader, val_loader=None):
-        training_losses = []
-
-        lr_steps = [int(k) for k in cfg.TRAIN.LEARNING_RATES.keys()]
-
-        #Setup the lr_scheduler
-        self.lr_scheduler = lr_scheduler.MultiStepLR(self.optimizer,
-                                                     lr_steps,
-                                                     gamma=0.1)
-
         train_loader_iter = iter(train_loader)
-        for train_step in tqdm(range(start_train_step, cfg.TRAIN.NUM_ITERATION + 1)):
-            self.lr_scheduler.step()
-
+        for train_step in tqdm(
+                range(self.start_train_step, cfg.TRAIN.NUM_ITERATION + 1)):
             try:
                 batch_img, batch_voxel = train_loader_iter.next()
             except StopIteration:
@@ -130,11 +154,12 @@ class TDR2N2Trainer(TDR2N2Detector):
                 batch_img, batch_voxel = train_loader_iter.next()
 
             data = self.trainStep(batch_img, batch_voxel)
+            self.scheduler.step()
             loss = data['losses']['loss']
             self.writer.add_scalar("train/loss", loss, train_step)
-            self.writer.add_scalar("train/lr", self.optimizer.param_groups[0]['lr'], train_step)
-
-            training_losses.append(loss.item())
+            self.writer.add_scalar("train/lr",
+                                   self.optimizer.param_groups[0]['lr'],
+                                   train_step)
 
             if train_step % cfg.TRAIN.PRINT_FREQ == 0:
                 print('%s Iter: %d Loss: %f' %
@@ -154,15 +179,50 @@ class TDR2N2Trainer(TDR2N2Detector):
 
             if (train_step % cfg.TRAIN.SAVE_FREQ == 0 and not train_step == 0) or \
                     (train_step == cfg.TRAIN.NUM_ITERATION):
-                self.writer.add_scalar("train/loss", training_losses, train_step)
                 self.saveModel(train_step)
-
-            #loss is a Variable containing torch.FloatTensor of size 1
-            if loss.item() > cfg.TRAIN.LOSS_LIMIT:
-                print("[ERROR][TDR2N2Trainer::train]")
-                print("\t cost exceeds the threshold! stop training!")
-                return False
         return True
 
+
+def cleanup_handle(func):
+    '''Cleanup the data processes before exiting the program'''
+
+    def func_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except:
+            print('Wait until the dataprocesses to end')
+            # kill_processes(train_queue, train_processes)
+            # kill_processes(val_queue, val_processes)
+            raise
+
+    return func_wrapper
+
+
+@cleanup_handle
 def demo():
+    #  cfg.DATASET = '/cvgl/group/ShapeNet/ShapeNetCore.v1/cat1000.json'
+    cfg.CONST.RECNET = 'rec_net'
+    cfg.TRAIN.DATASET_PORTION = [0, 0.8]
+
+    train_dataset = ShapeNetDataset(cfg.TRAIN.DATASET_PORTION)
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=cfg.CONST.BATCH_SIZE,
+                              shuffle=True,
+                              num_workers=cfg.TRAIN.NUM_WORKER,
+                              collate_fn=ShapeNetCollateFn(),
+                              pin_memory=True)
+
+    val_dataset = ShapeNetDataset(cfg.TEST.DATASET_PORTION)
+    val_collate_fn = ShapeNetCollateFn(train=False)
+    val_loader = DataLoader(dataset=val_dataset,
+                            batch_size=cfg.CONST.BATCH_SIZE,
+                            shuffle=True,
+                            num_workers=1,
+                            collate_fn=val_collate_fn,
+                            pin_memory=True)
+
+    model_file_path = "./output/models/checkpoint.pth"
+
+    td_r2n2_trainer = TDR2N2Trainer(model_file_path)
+    td_r2n2_trainer.train(train_loader, val_loader)
     return True
