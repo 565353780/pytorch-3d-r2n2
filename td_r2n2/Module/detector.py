e99ecd14e92f353b71cad8c58eacc4161af67044
chLi <565353780@qq.com>
2022-10-12 15:33:52 +0800

    remove unused code

--- td_r2n2/Module/detector.py
+++ td_r2n2/Module/detector.py
@@ -6,7 +6,7 @@ import torch
 import numpy as np
 from PIL import Image
 
-from td_r2n2.Config.config import cfg, cfg_from_list
+from td_r2n2.Config.config import cfg
 
 from td_r2n2.Model.res_gru.res_gru_net import ResidualGRUNet
 
@@ -78,26 +78,3 @@ class TDR2N2Detector(object):
         voxel2obj(save_obj_file_path,
                   voxel_prediction[0, 1] > cfg.TEST.VOXEL_THRESH)
         return True
-
-
-def demo():
-    model_file_path = "/home/chli/chLi/3D-R2N2/checkpoint.pth"
-    #  model_file_path = "./output/models/checkpoint.pth"
-
-    #  image_folder_path = "./images/"
-    image_folder_path = "/home/chli/chLi/3D-R2N2/test_images/screen/white/"
-
-    save_obj_file_path = "/home/chli/chLi/3D-R2N2/test_images/screen/screen.obj"
-
-    cfg_from_list(['CONST.BATCH_SIZE', 1])
-
-    td_r2n2_detector = TDR2N2Detector(model_file_path)
-
-    image_file_path_list = []
-    image_file_name_list = os.listdir(image_folder_path)
-    for image_file_name in image_file_name_list:
-        image_file_path = image_folder_path + image_file_name
-        image_file_path_list.append(image_file_path)
-    data = td_r2n2_detector.detectImageFiles(image_file_path_list)
-    td_r2n2_detector.saveAsObj(data, save_obj_file_path)
-    return True
