45ac85b519d2095b659c5c80f4dfb9a7657134f7
chLi <565353780@qq.com>
2022-10-12 20:28:11 +0800

    fix demo import bugs

--- demo.py
+++ demo.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from td_r2n2.Module.detector import demo
+from td_r2n2.Demo.detector import demo as demo_detect
+from td_r2n2.Demo.trainer import demo as demo_train
 
 if __name__ == '__main__':
-    demo()
+    demo_detect()
+    #  demo_train()
