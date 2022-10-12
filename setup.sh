bc77e16ba86e4e2b77201749fc1b3ddf0ccd6359
chLi <565353780@qq.com>
2022-10-12 20:28:16 +0800

    update setup bash files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-pip install numpy EasyDict Pillow pyyaml sklearn tqdm
+pip install numpy EasyDict Pillow pyyaml sklearn tqdm opencv-python
 pip install torch torchvision torchaudio \
   --extra-index-url https://download.pytorch.org/whl/cu113
 
